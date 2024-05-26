df6e387bf7488f534af37e7acadd3fe25dbbb01c
Fafakofi <sfkbenson@st.ug.edu.gh>
2024-05-26 17:28:14 +0000

    background color and a few other thing changed

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is Steve</Text> 
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#756'
   },
+
+  Text: {
+    fontSize: 240,
+    fontWeight:'bold'
+  }
 });
